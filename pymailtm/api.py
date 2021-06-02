0facf05f7d2b6922516a52b958c8e3105e3a5a86
Carlo De Pieri <depieri.carlo@gmail.com>
2021-06-02 11:39:31 +0200

    add Account creation happy path

--- pymailtm/api.py
+++ pymailtm/api.py
@@ -1,7 +1,13 @@
+import json
+import random
 import requests
+import string
 
+from random_username.generate import generate_username
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Dict, List, Union
+
+from requests.models import Response
 
 
 api_address = "https://api.mail.tm"
@@ -16,6 +22,20 @@ class Domain:
     isPrivate: bool
     createdAt: str
     updatedAt: str
+
+
+@dataclass
+class Account:
+    """An account resource from the mail.tm web api."""
+    id: str
+    address: str
+    quota: int
+    used: 0
+    isDisabled: bool
+    isDeleted: bool
+    createdAt: str
+    updatedAt: str
+    password: str
 
 
 class DomainManager:
@@ -39,7 +59,7 @@ class DomainManager:
         return DomainManager._domain_from_dict(response)
 
     @staticmethod
-    def _domain_from_dict(domain_data: Dict):
+    def _domain_from_dict(domain_data: Dict) -> Domain:
         """Return a new Domain object starting from a dict of data."""
         return Domain(
             domain_data["id"],
@@ -49,3 +69,67 @@ class DomainManager:
             domain_data["createdAt"],
             domain_data["updatedAt"]
         )
+
+
+class AccountManager:
+    """TODO"""
+
+    @staticmethod
+    def new(user: Union[str, None] = None,
+            domain: Union[str, None] = None,
+            password: Union[str, None] = None) -> Account:
+        """Create an account on mail.tm."""
+        address = AccountManager.generate_address(user, domain)
+        if password is None:
+            password = AccountManager._generate_password(6)
+
+        account = {"address": address, "password": password}
+        headers = {
+            "accept": "application/ld+json",
+            "Content-Type": "application/json"
+        }
+        r = requests.post("{}/{}".format(api_address, "accounts"),
+                          data=json.dumps(account), headers=headers)
+
+        data = r.json()
+        data["password"] = password
+        return AccountManager._account_from_dict(data)
+
+    @staticmethod
+    def _account_from_dict(data: Dict) -> Account:
+        return Account(
+            id=data["id"],
+            address=data["address"],
+            quota=data["quota"],
+            used=data["used"],
+            isDisabled=data["isDisabled"],
+            isDeleted=data["isDeleted"],
+            createdAt=data["createdAt"],
+            updatedAt=data["updatedAt"],
+            password=data["password"]
+        )
+
+    @staticmethod
+    def generate_address(user: Union[str, None] = None, domain: Union[str, None] = None) -> str:
+        """Generate an address. 
+
+        Will raise DomainNotAvailableException when trying to use an unavailable domain."""
+        valid_domains = DomainManager.getActiveDomains()
+        if domain is None:
+            domain = valid_domains[0].domain
+        else:
+            if len(list(filter(lambda x: x.domain == domain, valid_domains))) == 0:
+                raise DomainNotAvailableException()
+        if user is None:
+            user = generate_username(1)[0].lower()
+        return f"{user}@{domain}"
+
+    @staticmethod
+    def _generate_password(length):
+        """Generate a random alphanumeric password of the given length."""
+        letters = string.ascii_letters + string.digits
+        return ''.join(random.choice(letters) for _ in range(length))
+
+
+class DomainNotAvailableException(Exception):
+    """Exception raised when trying to use an unavailable domain in an address."""
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,10 @@
-from typing import List
 import pytest
-from pymailtm.api import Domain, DomainManager
+import re
+
+from typing import List
+
+from random_username.generate import generate_username
+from pymailtm.api import Account, AccountManager, Domain, DomainManager, DomainNotAvailableException
 
 
 class TestADomain:
@@ -45,3 +49,91 @@ class TestADomainManager:
         domain = self.domains[0]
         domain_data = DomainManager.getDomain(domain.id)
         assert domain.domain == domain_data.domain
+
+
+class TestAnAccount:
+    """Test: An Account..."""
+
+    def test_should_save_all_required_fields(self):
+        """It should save all required fields"""
+        id_ = "000011"
+        address = "account@testdomain.com"
+        quota = 40000
+        used = 0
+        isDisabled = True
+        isDeleted = False
+        createdAt = "2021-05-22T00:00:00+00:00"
+        updatedAt = "2021-05-22T00:00:00+00:00"
+        password = "secure"
+
+        test_account = Account(id_, address, quota, used, isDisabled, isDeleted, createdAt, updatedAt, password)
+
+        assert test_account.id == id_
+        assert test_account.address == address
+        assert test_account.quota == quota
+        assert test_account.used == used
+        assert test_account.isDisabled == isDisabled
+        assert test_account.isDeleted == isDeleted
+        assert test_account.createdAt == createdAt
+        assert test_account.updatedAt == updatedAt
+        assert test_account.password == password
+
+
+class TestAnAccountManager:
+    """Test: An AccountManager..."""
+
+    def test_should_be_able_to_generate_a_valid_address_with_no_arguments(self):
+        """It should be able to generate a valid address with no arguments"""
+        address = AccountManager.generate_address()
+        reg = r"[^@]+@[^@]+\.[^@]+"
+        assert re.fullmatch(reg, address) is not None
+
+    def test_should_be_able_to_generate_a_valid_address_with_the_provided_arguments(self):
+        """It should be able to generate a valid address with the provided arguments"""
+        valid_domain = DomainManager.getActiveDomains()[0]
+        address = AccountManager.generate_address(user="nick", domain=valid_domain.domain)
+        assert address == f"nick@{valid_domain.domain}"
+
+    def test_should_raise_an_error_with_an_invalid_domain(self):
+        """It should raise an error with an invalid domain"""
+        with pytest.raises(DomainNotAvailableException):
+            AccountManager.generate_address(user="nick", domain="invalid.nope")
+
+    def test_should_be_able_to_generate_a_random_password(self):
+        """It should be able to generate a random password"""
+        password = AccountManager._generate_password(6)
+        assert len(password) == 6
+        assert type(password) is str
+
+    def test_should_be_able_to_create_an_account_from_a_dict(self):
+        """It should be able to create an Account from a dict"""
+        data = {"id": "000011",
+                "address": "account@testdomain.com",
+                "quota": 40000,
+                "used": 0,
+                "isDisabled": True,
+                "isDeleted": False,
+                "createdAt": "2021-05-22T00:00:00+00:00",
+                "updatedAt": "2021-05-22T00:00:00+00:00",
+                "password": "secure"
+                }
+        account = AccountManager._account_from_dict(data)
+        assert isinstance(account, Account)
+        assert account.id == data["id"]
+
+    def test_should_be_able_to_create_an_account_with_no_arguments(self):
+        """It should be able to create an account with no arguments"""
+        account = AccountManager.new()
+        assert isinstance(account, Account)
+        assert len(account.address) > 0
+        assert len(account.password) == 6
+
+    def test_should_be_able_to_create_an_account_with_the_specified_arguments(self):
+        """It should be able to create an account with the specified arguments"""
+        user = generate_username(1)[0].lower()
+        domain = DomainManager.getActiveDomains()[0].domain
+        password = "secure"
+        account = AccountManager.new(user=user, domain=domain, password=password)
+        assert isinstance(account, Account)
+        assert account.address == f"{user}@{domain}"
+        assert account.password == password
